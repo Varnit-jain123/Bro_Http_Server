d1fb9949fa1fc6d4900ee45ef8554103e1241d77
Varnit Jain <varnitjaintj@gmail.com>
2026-06-01 21:05:28 +0530

    finalize things

--- BroServer/shutdown_script/shutdown_bro.cpp
+++ BroServer/shutdown_script/shutdown_bro.cpp
@@ -1,17 +1,37 @@
 #include<iostream>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<arpa/inet.h>
 #include<string.h>
+
+#ifdef _WIN32
+    #include<winsock2.h>
+    #include<ws2tcpip.h>
+    #define CLOSE_SOCKET(s) closesocket(s)
+    typedef int socklen_t;
+#else
+    #include<sys/socket.h>
+    #include<unistd.h>
+    #include<arpa/inet.h>
+    #define CLOSE_SOCKET(s) close(s)
+#endif
+
 using namespace std;
+
 int main()
 {
+    #ifdef _WIN32
+        WSADATA wsaData;
+        if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
+        {
+            cout<<"WSAStartup failed"<<endl;
+            return 1;
+        }
+    #endif
+
     int success;
     int x;
     int portNumber=6060;
     int socketDescriptor;
     struct sockaddr_in serverAddress;
-    char messageToSend[]="GET /shutdown HTTP/1.1\r\nHost: localhost:6060\r\nAccept: text/html\r\n";
+    char messageToSend[]="GET /shutdown HTTP/1.1\r\nHost: localhost:6060\r\nAccept: text/html\r\n\r\n";
     char responseBuffer[1024];
     x=1;
     while(x<=2)
@@ -20,6 +40,9 @@ int main()
         if(socketDescriptor<0)
         {
             cout<<"Socket creation failed"<<endl;
+            #ifdef _WIN32
+                WSACleanup();
+            #endif
             return 1;
         }
         serverAddress.sin_family=AF_INET;
@@ -28,21 +51,30 @@ int main()
         if(success<=0)
         {
             cout<<"Unable to resolve 127.0.0.1"<<endl;
-            close(socketDescriptor);
+            CLOSE_SOCKET(socketDescriptor);
+            #ifdef _WIN32
+                WSACleanup();
+            #endif
             return 1;
         }
         success=connect(socketDescriptor,(struct sockaddr *)&serverAddress,sizeof(serverAddress));
         if(success<0)
         {
-            cout<<"Connection failed"<<endl;
-            close(socketDescriptor);
+            cout<<"Connection failed - is the server running on port "<<portNumber<<"?"<<endl;
+            CLOSE_SOCKET(socketDescriptor);
+            #ifdef _WIN32
+                WSACleanup();
+            #endif
             return 1;
         }
         send(socketDescriptor,messageToSend,strlen(messageToSend),0);
-        read(socketDescriptor,responseBuffer,1024);
-        close(socketDescriptor);
+        recv(socketDescriptor,responseBuffer,1024,0);
+        CLOSE_SOCKET(socketDescriptor);
         x++;
     }
-    cout<<"Shutdown initiated"<<endl;
+    cout<<"Shutdown initiated successfully"<<endl;
+    #ifdef _WIN32
+        WSACleanup();
+    #endif
     return 0;
 }
